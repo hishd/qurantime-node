258541ccc9d98ad154d5d63ba62f707a905e84e4
Hishara Dilshan <hish2k15@gmail.com>
2021-10-22 00:27:49 +0530

    Ready to deploy on heroku

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,24 @@ connectDB()
 const app = express()
 app.use(express.json())
 
-const __dirname = path.resolve()
-
 app.use('/api/healthStatus', healthStatusRoutes)
 app.use('/api/hospital', hospitalRoutes)
 app.use('/api/location', locationRoutes)
 app.use('/api/officer', officerRoutes)
 app.use('/api/patient', patientRoutes)
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
