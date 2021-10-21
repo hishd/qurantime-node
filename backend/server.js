9bac109a82a556a417739c7ded90d8ad98cc9658
Hishara Dilshan <hish2k15@gmail.com>
2021-10-21 22:11:33 +0530

    Preparation for production

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,24 @@ connectDB()
 const app = express()
 app.use(express.json())
 
+const __dirname = path.resolve()
+
 app.get('/', (req, res) => {
   res.send('Qurantime API is running......!!')
 })
+
+//Preparing for production & deployment
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
 
 app.use('/api/healthStatus', healthStatusRoutes)
 app.use('/api/hospital', hospitalRoutes)
